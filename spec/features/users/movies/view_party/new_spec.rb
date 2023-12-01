a83cf33b1bfb250d7399d212672aa8cce766bd71
Justin Lee <140270202+JustinSteel@users.noreply.github.com>
2023-11-30 19:46:39 -0700

    add new viewparty test

--- spec/features/users/movies/view_party/new_spec.rb
+++ spec/features/users/movies/view_party/new_spec.rb
@@ -3,10 +3,24 @@ require "rails_helper"
 RSpec.describe "View Party New Page", type: :feature do
   before :each do
     @user = User.create(name: "Bob", email: "bob@gmail.com")
+    @user2 = User.create(name: "Sally", email: "sally@gmail.com")
+    @user3 = User.create(name: "Joe", email: "joe@gmail.com")
   end
   describe "create a new view party" do
     it "should have content", :vcr do
-      visit "/users/#{@user.id}/"
+      visit "/users/#{@user.id}/movies/550/viewing-party/new"
+      expect(page).to have_content("Create a Viewing Party for Fight Club")
+      expect(page).to have_button("Discover Page")
+      within("#party_details") do
+        expect(page).to have_content("Movie Title: Fight Club")
+        expect(page).to have_content("Duration of Party: 139")
+        expect(page).to have_content("Date of Party:")
+        expect(page).to have_content("Start Time:")
+        expect(page).to have_content("Invite Other Users")
+        expect(page).to have_checked_field("Sally (sally@gmail.com)")
+        expect(page).to have_checked_field("Joe (joe@gmail.com)")
+        expect(page).to have_button("Create Party")
+      end
     end
   end
 end
